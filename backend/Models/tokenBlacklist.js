c21fc9d67b784954e6193c8ca2b47ee2063ca115
vityasyyy <argyavityasy1208@gmail.com>
2024-08-22 15:10:35 +0700

    [FEAT](Models/): Added token blacklisting to handle user logout so user cannot use the same token after logging out

---
+++ backend/Models/tokenBlacklist.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const blacklistedTokenSchema = new mongoose.Schema({
+  token: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  expiresAt: {
+    type: Date,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('BlacklistedToken', blacklistedTokenSchema);
