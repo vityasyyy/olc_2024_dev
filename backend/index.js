79a6df7191fe3cebc67a82d5765a66d8c71a6a56
vityasyyy <argyavityasy1208@gmail.com>
2024-08-22 15:13:30 +0700

    [FEAT](index.js): Added initial index.js for entrypoint of the app

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const cors = require('cors');
+const connectDB = require('./Models/connectDB');
+const userRoutes = require('./Routes/userRoutes');
+
+const app = express();
+app.set('trust proxy', 1);
+
+connectDB();
+
+app.use(helmet());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
+  credentials: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(mongoSanitize());
+
+app.use('/auth', userRoutes);
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500 } = err;
+  if(!err.message) err.message = "Something went wrong";
+  res.status(statusCode).json({ error: err.message });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
