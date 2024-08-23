b10952e51213a8b4903cb4424b37efc2a2c8e37d
vityasyyy <argyavityasy1208@gmail.com>
2024-08-23 13:20:42 +0700

    [FEAT](Models/): added function to connect to the database

---
+++ backend/Models/connectDB.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const mongoose = require('mongoose');
+
+module.exports = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB connected...');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1);
+    }
+};
