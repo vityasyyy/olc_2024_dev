0355d9f147b9c3e8d1bd0854f6db2acb9957a058
vityasyyy <argyavityasy1208@gmail.com>
2024-08-23 13:21:37 +0700

    [FEAT](/): added Routes directory and user routes fike for auth routes handling

---
+++ backend/Routes/userRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router({mergeParams: true})
+const {login, register, logout, validate, requestPasswordReset, resetPassword} = require('../Controllers/userController');
+const {isAuthenticated} = require('../Middlewares/middlewares')
+router.post('/register', register);
+router.post('/login', login);
+router.post('/logout', isAuthenticated, logout);
+router.get('/validate', isAuthenticated, validate);
+router.post('/request-password-reset', requestPasswordReset);
+router.post('/reset-password', resetPassword);
+module.exports = router;
