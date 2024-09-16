bcbe819eb8de86e761af85a396193ed363e5e976
vityasyyy <argyavityasy1208@gmail.com>
2024-09-16 19:34:09 +0700

    feat(routes): added more routes for class fetching

--- backend/Routes/olclassRoutes.js
+++ backend/Routes/olclassRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-const {enroll} = require('../Controllers/olclassController');
+const {enroll,getAllClasses, getOneClass} = require('../Controllers/olclassController');
 const {isAuthenticated} = require('../Utils/middlewares');
 
+
+router.get('/', getAllClasses);
+router.get('/:id', getOneClass);
 router.post('/:id/enroll', isAuthenticated, enroll);
 
 module.exports = router;
