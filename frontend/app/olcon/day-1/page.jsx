afdd4c7edf3cfbf117f9662b31b27d3351f5800f
Devin <sultandevinosuyudi@mail.ugm.ac.id>
2024-10-11 23:37:42 +0700

    feat: - added suspense loading for olcon - added images lol

--- frontend/app/olcon/day-1/page.jsx
+++ frontend/app/olcon/day-1/page.jsx
@@ -1,9 +1,15 @@
 import OLConPage from "@/components/olcon/Page";
+import LoadingUI from "@/app/olcon/LoadingUI";
+import { Suspense } from "react";
 
 // ACTUAL DAY
 const DAY = 1;
 
 export default function Page() {
-  // minus 1 because of index starts at 0
-  return <OLConPage DAY={DAY - 1} />;
+  // minus 1 because index starts at 0
+  return (
+    <Suspense fallback={<LoadingUI />}>
+      <OLConPage DAY={DAY - 1} />;
+    </Suspense>
+  );
 }
