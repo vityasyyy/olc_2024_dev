9b7982b1175add588c7622620cce38023632f9ec
Argya Vityasy <149230734+vityasyyy@users.noreply.github.com>
2024-10-19 00:45:54 +0700

    Update CardLoading.jsx

--- frontend/components/class/CardLoading.jsx
+++ frontend/components/class/CardLoading.jsx
@@ -19,7 +19,7 @@ const CardLoading = () => {
       </div>
 
       <div className="flex flex-col gap-2 p-3">
-        <Skeleton className="h-8 w-full" />
+        <Skeleton className="h-8 w-full " />
         <Skeleton className="h-8 w-full" />
         <Skeleton className="h-4 w-1/2" />
         <Button
